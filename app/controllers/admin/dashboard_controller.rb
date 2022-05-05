fe993a9f2cf2ad85810931572da092fde3f0e9ec
Calvin Chung <calvin-chung@live.com>
2022-05-04 22:32:32 -0700

    admin page displays number of products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class Admin::DashboardController < ApplicationController
+
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
   password: ENV["HTTP_BASIC_PASSWORD"]
+
   def show
+    @product = Product.all.count
+    @categories = Category.all.count
   end
+
 end
